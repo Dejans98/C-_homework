898bae78934d72d0787a341dcfa5cad0503b4e6f
Dejans98 <spasovskid98@gmail.com>
2025-03-13 15:00:20 +0100

    Homework 2

---
+++ C# homework 2/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            int sumOfEven = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Enter integer no.{i + 1}:");
+                numbers[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (numbers[i] % 2 == 0)  
+                {
+                    sumOfEven += numbers[i]; 
+                }
+            }
+
+            
+            Console.WriteLine("The result is: " + sumOfEven);
+
+
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ C# homework 2/Solution1/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 =  { "Ratko","Petko","Boshko","Mile","Zoki" };
+            string[] studentsG2 =  {"Dejan","Petar","Filip","Stefan","Bube" };
+
+            Console.WriteLine("Enter a student group 1 or 2:");
+            string input = Console.ReadLine();
+            
+                if (input =="1")
+                {
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    string student = studentsG1[i];
+                    Console.WriteLine(student);
+                }    
+                }
+
+                else if (input == "2")
+                {
+                for (int i =0;i<studentsG2.Length;i++)
+                {
+                string student = studentsG2[i];
+                    Console.WriteLine(student);    
+                }
+                }
+
+                else
+                {
+                Console.WriteLine("Invalid number please enter 1 or 2: ");
+                }
+
+
+
+
+
+
+
+
+      
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
